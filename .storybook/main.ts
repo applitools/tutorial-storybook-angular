501d0a5b61083b607d3863edb823e6cb69ed25a8
isonic1 <justin.ison@gmail.com>
2025-05-01 10:50:40 -0400

    updates to latest versions

---
+++ .storybook/main.ts
@@ -0,0 +1,23 @@
+import { dirname, join } from "path";
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-interactions")
+  ],
+  "framework": {
+    "name": "@storybook/angular",
+    "options": {}
+  }
+};
+export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
---
+++ applitools.config.js
@@ -0,0 +1,13 @@
+// See https://applitools.com/tutorials/sdks/storybook/config#properties for additional configuration properties.
+module.exports = {
+  apiKey: process.env.APPLITOOLS_API_KEY,
+  serverUrl: 'https://eyes.applitools.com',
+  batchName: 'Storybook Angular Tutorial',
+  testConcurrency: 10,
+  showLogs: false,
+  browser: [
+    { width: 1024, height: 768, name: 'chrome' }, // See https://applitools.com/tutorials/sdks/storybook/browsers for additional supported browsers and devices.
+    // { width: 1024, height: 768, name: 'firefox' },
+    // { chromeEmulationInfo: { deviceName: 'Galaxy S20' } },
+ ] 
+}
